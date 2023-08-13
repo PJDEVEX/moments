d6b1b277017417ea3535fd83a6c9e4de3bae4125
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-13 17:41:16 +0000

    Bug fix: server url correction

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,14 +1,14 @@
 import axios from "axios";
 
 // Global cofig of axios
-axios.defaults.baseURL = "https://moments-ci-af-df0edfddd1f4.herokuapp.com/";
+axios.defaults.baseURL = "https://ci-drf-api-pj-87220ee25e96.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
 //  creating an instance of the axios client with specific configurations
 // Reccommended for multiple API fetching
 // const instance = axios.create({
-//   baseURL: 'https://moments-ci-af-df0edfddd1f4.herokuapp.com/',
+//   baseURL: 'https://ci-drf-api-pj-87220ee25e96.herokuapp.com/',
 //   headers: {
 //     'Content-Type': 'multipart/form-data',
 //   },
