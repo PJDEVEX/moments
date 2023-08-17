02ed1840bb4093741d259549b25b463da8050d61
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-17 21:58:08 +0000

    Import Post component for rendering individual posts on PostPage

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -7,6 +7,7 @@ import Container from "react-bootstrap/Container";
 import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
 
 function PostPage() {
 // Use useParams to extract post_id
@@ -35,7 +36,8 @@ useEffect(() => {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <p>Post component</p>
+        {/* (4)Passing props from PostPage.js */}
+        <Post {...post.results[0]} setPosts={setPost} postPage/>
         <Container className={appStyles.Content}>
           Comments
         </Container>
