b6a6f56e127569416d24f5a4e73d116098a42c22
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-17 00:32:22 +0000

    Add PostCreateForm

---
+++ src/pages/posts/PostCreateForm.js
@@ -0,0 +1,139 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from 'react-bootstrap/Image';
+
+import Upload from "../../assets/upload.png";
+
+import styles from "../../styles/PostCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
+
+function PostCreateForm() {
+  const [errors, setErrors] = useState({});
+
+  // (8.1) Create and initialize Post Data values
+  const [postData, setPostData] = useState({
+    title: "",
+    content: "",
+    image: "",
+  });
+
+  // (8.2) Destructure post data
+  const { title, content, image } = postData;
+
+  // (10) handleChange function
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // (12) handleChangeImage function
+  const handleChangeImage = (event) => {
+    const selectedFile = event.target.files.length;
+    if (selectedFile) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+  const textFields = (
+    <div className="text-center">
+      {/* (7) Add input fields */}
+      <Form.Group controlId="postTitle">
+        <Form.Label>Title</Form.Label>
+        {/* (9) set post data values and on change*/}
+        <Form.Control
+          type="text"
+          name="title"
+          value={title}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Form.Group controlId="postContent">
+        <Form.Label>Content</Form.Label>
+        {/* (9) set post data values and onChange */}
+        <Form.Control
+          as="textarea"
+          name="content"
+          rows={6}
+          value={content}
+          onChange={handleChange}
+        />
+      </Form.Group>
+
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        onClick={() => {}}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        create
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              {/* (13) Show the preview of the image if one is chosen */}
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  {/* (6)ASSET */}
+                  <Asset src={Upload} message={"Click or tap to upload"} />
+                </Form.Label>
+              )}
+
+              {/* (11) Adding onChange props */}
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+              />
+            </Form.Group>
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default PostCreateForm;
