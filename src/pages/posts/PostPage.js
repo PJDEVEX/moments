921b02b62f98dcfb88bb4171196e2e887dfc65be
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-19 21:24:25 +0000

    Add create comment functionality

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -9,37 +9,56 @@ import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Post from "./Post";
 
-function PostPage() {
-// Use useParams to extract post_id
-const {id} = useParams();
-// Initializing post and setPost state
-const[post, setPost] = useState({results: []});
+// (3) import CommentCreateForm and useCurrentUser
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../components/contexts/CurrentUserContext";
 
-// Using useEffect and async function to Fetch post data
-useEffect(() => {
+function PostPage() {
+  // Use useParams to extract post_id
+  const { id } = useParams();
+  // Initializing post and setPost state
+  const [post, setPost] = useState({ results: [] });
+
+  // (4) Declare currentUser, Profile_image, comments variables
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
+
+  // Using useEffect and async function to Fetch post data
+  useEffect(() => {
     const handleMount = async () => {
       try {
-        const [{data: post}] = await Promise.all ([
-            axiosReq.get(`/posts/${id}`)
-        ])
+        const [{ data: post }] = await Promise.all([
+          axiosReq.get(`/posts/${id}`),
+        ]);
         setPost({ results: [post] });
-        console.log('Fetched Post:', post);
+        console.log("Fetched Post:", post);
       } catch (error) {
-        console.error('Error fetching post:', error);
+        console.error("Error fetching post:", error);
       }
     };
     handleMount();
   }, [id]);
-  
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
         {/* Passing props from PostPage.js */}
-        <Post {...post.results[0]} setPosts={setPost} postPage/>
+        <Post {...post.results[0]} setPosts={setPost} postPage />
         <Container className={appStyles.Content}>
-          Comments
+          {/* Conditionally allow to comment */}
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              post={id}
+              setPost={setPost}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
