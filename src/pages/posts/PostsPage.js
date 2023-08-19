bd7783dac17cd9eae4af72b4f86fe8bc26f534fb
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-19 10:12:27 +0000

    Add search bar functionality

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -4,29 +4,31 @@ import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
-import Post from "./Post"; 
-import NoResults from "../../assets/no-results.png"; 
-import Asset from "../../components/Asset"; 
+import Post from "./Post";
+import NoResults from "../../assets/no-results.png";
+import Asset from "../../components/Asset";
 
 import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
-import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
-import { axiosReq } from "../../api/axiosDefaults.js"; 
+import { useLocation } from "react-router-dom";
+import { axiosReq } from "../../api/axiosDefaults.js";
 
 // Destructure the message and filter  and initiate to empty string
-function PostsPage(message, filter = "") {
+function PostsPage({ message, filter = "" }) {
   // Initialize state for posts
   const [posts, setPosts] = useState({ results: [] });
   // Initialize state to track whether the posts have loaded
   const [hasLoaded, setHasLoaded] = useState(false);
   // Access the current pathname from the location object
   const { pathname } = useLocation();
+  //   (3)Initialize state for search query
+  const [query, setQuery] = useState("");
 
   // Fetching Posts Based on Filter
   useEffect(() => {
     const fetchPosts = async () => {
       try {
-        const { data } = await axiosReq.get(`/posts/?${filter}`);
+        const { data } = await axiosReq.get(`/posts/?${filter}search=${query}`); // (4) Fetch posts with the given filter and search query
         // Update the 'posts' state with the fetched data
         setPosts(data);
         // Set the flag to indicate that posts have loaded
@@ -42,13 +44,32 @@ function PostsPage(message, filter = "") {
     fetchPosts();
 
     // useEffect hook with dependencies 'filter' and 'pathname',
+    // (4) add query too
     // this will trigger the effect whenever filter or pathname changes
-  }, [filter, pathname]);
+  }, [filter, query, pathname]);
 
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
+        {/* (1) Add search bar */}
+        <i className={`fas fa-search ${styles.SearchIcon}`} />
+        {/* Create a form for the search bar */}
+        <Form
+          className={styles.SearchBar}
+          onSubmit={(event) => event.preventDefault()}
+        >
+          {/* (2) Input field for searching */}
+          <Form.Control
+            type="text"
+            className="mr-sm-2"
+            placeholder="Search posts"
+            // Bind input value to 'query' state
+            value={query}
+            // Update 'query' state on input change
+            onChange={(event) => setQuery(event.target.value)}
+          ></Form.Control>
+        </Form>
         {/* List of posts contextually */}
         {hasLoaded ? (
           <>
