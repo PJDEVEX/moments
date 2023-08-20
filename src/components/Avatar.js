60da0080dd08a227f5eddc77d367727e67f08798
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-20 08:04:47 +0000

    Bug fix: Correct Avatar component image rendering

--- src/components/Avatar.js
+++ src/components/Avatar.js
@@ -1,14 +1,19 @@
-// (5) Avatar component for the profile image
 import React from "react";
 import styles from "../styles/Avatar.module.css";
 
-function Avatar({ src, height = 45, text }) {
+const Avatar = ({ src, height = 45, text }) => {
   return (
-    <span className={styles.Avatar}>
-      <img src={src} height={height} alt="avatar" width={height} />
+    <span>
+      <img
+        className={styles.Avatar}
+        src={src}
+        height={height}
+        width={height}
+        alt="avatar"
+      />
       {text}
     </span>
   );
-}
+};
 
 export default Avatar;
