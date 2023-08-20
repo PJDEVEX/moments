9f146ef8669f8fc13d4b937b259ee2b98fa36141
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-08-20 21:10:02 +0000

    Bug fix: file path correction

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,30 +1,30 @@
 import { Container } from "react-bootstrap";
-import appStyles from "../../App.module.css"; 
-import Asset from "../../components/Asset"; 
-import Profile from "../posts/Profile";
+import appStyles from "../../App.module.css";
+import Asset from "../../components/Asset";
+import Profile from "./Profile";
 import { useProfileData } from "../../contexts/ProfileDataContext";
 
 // pass mobile param
 const PopularProfiles = ({ mobile }) => {
-   // ReoveDestructure the profileData object
+  // ReoveDestructure the profileData object
   //  const { popularProfiles } = profileData;
   const { popularProfiles } = useProfileData();
-
 
   return (
     // Conditionally rendering popProfiles and add styling accordingly
     // Pass profile component
-    <Container className={`${appStyles.Content} ${
-      mobile && "d-lg-none text-center mb-3"
-    }`}
-  >
+    <Container
+      className={`${appStyles.Content} ${
+        mobile && "d-lg-none text-center mb-3"
+      }`}
+    >
       {popularProfiles.results.length ? (
         <>
           <p>Most followed profiles.</p>
           {mobile ? (
             <div className="d-flex justify-content-around">
               {popularProfiles.results.slice(0, 4).map((profile) => (
-                <Profile key={profile.id} profile={profile} mobile/>
+                <Profile key={profile.id} profile={profile} mobile />
               ))}
             </div>
           ) : (
@@ -34,7 +34,7 @@ const PopularProfiles = ({ mobile }) => {
           )}
         </>
       ) : (
-        <Asset spinner={true} /> 
+        <Asset spinner={true} />
       )}
     </Container>
   );
